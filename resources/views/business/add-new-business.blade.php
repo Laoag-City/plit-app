fb354730add691f9ec1e3c26b04025b8f9c8f6e5
Russell James Bello <30307544+RussellJamesBello@users.noreply.github.com>
2024-03-15 16:36:04 +0800

    Initial map JavaScript logic

--- resources/views/business/add-new-business.blade.php
+++ resources/views/business/add-new-business.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:title>Add New Business</x-slot>
 
-    <form action="{{ url()->current() }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ url()->current() }}" method="POST" enctype="multipart/form-data" x-data="form">
         @csrf
 
         @if($errors->any())
@@ -64,6 +64,16 @@
                 :multiple="true"
             />
 
+            
+
+            <div id="map-container" class="lg:col-span-2 hidden">
+                <h3 class="text-left font-bold">Tag business location</h3>
+
+                <div id="map" class="h-96"></div>
+
+                <input type="hidden" name="coordinates" x-model="coordinates">
+            </div>
+
             <div class="lg:col-span-2 mt-5">
                 <x-actions.button
                     text="Submit Form"
@@ -72,4 +82,55 @@
             </div>
         </div>
     </form>
+
+    @pushOnce('scripts')
+        <script>
+            document.addEventListener('alpine:init', () => {
+                Alpine.data('form', () => ({
+                    map: null,
+                    marker: null,
+                    coordinates: {{ Js::from(old('coordinates')) }},
+
+                    init(){
+                        navigator.geolocation.getCurrentPosition((position) => {
+                            document.getElementById('map-container').classList.remove('hidden');
+
+                            var initial_coordinates = [];
+
+                            if(this.coordinates != null)
+                            {
+                                initial_coordinates[0] = _.split(this.coordinates, ' ')[0];
+                                initial_coordinates[1] = _.split(this.coordinates, ' ')[1];
+                            }
+
+                            else
+                            {
+                                initial_coordinates[0] = position.coords.latitude;
+                                initial_coordinates[1] = position.coords.longitude;
+
+                                this.coordinates = position.coords.latitude + ' ' + position.coords.longitude;
+                            }
+
+                            this.map = L.map('map')
+                                        .setView([initial_coordinates[0], initial_coordinates[1]], 15)
+                                        .on('click', (e) => {
+                                            this.map.removeLayer(this.marker);
+                                            this.marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map);
+                                            this.coordinates = this.marker._latlng.lat + ' ' + this.marker._latlng.lng;
+                                        });
+
+                            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                maxZoom: 19,
+                                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                            }).addTo(this.map);
+
+                            this.marker = L.marker([initial_coordinates[0], initial_coordinates[1]]).addTo(this.map);
+                        }, () => {
+                            window.alert('Location service is blocked. Please enable it in your browser\'s site settings.');
+                        });
+                    }
+                }));
+            });
+        </script>
+    @endPushOnce
 </x-layout>
