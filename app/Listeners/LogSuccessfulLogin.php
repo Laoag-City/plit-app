6c36e91adf2b92290a9846d9f4a2dfa4a7db5ca5
Russell James Bello <30307544+RussellJamesBello@users.noreply.github.com>
2024-03-26 15:13:09 +0800

    Year checking feature for marking businesses as under renewal and resetting permit values

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Services\YearService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        (new YearService)->checkYear();
+    }
+}
--- app/Services/YearService.php
+++ app/Services/YearService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Year;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class YearService
 {
@@ -11,30 +12,57 @@ class YearService
 
 	public function __construct()
 	{
-		$this->now = Carbon::now()->year;
+		$this->now = Carbon::now()->year;//dd($this->now);
 	}
 
 	public function checkYear()
 	{
-		//check if the year now is already recorded
-		if(Year::where('year', $this->now)->count() == 0)
+		$latest_year = Year::latest('year')->first();
+
+		//if none is saved in the database yet, initialize the year now
+		if($latest_year == null)
 		{
 			$year = new Year;
 			$year->year = $this->now;
 			$year->save();
 		}
-
-		else
-			$year = Year::where('year', $this->now)->first();
-	}
-
-	public function isNewYear()
-	{
-		$latest_year = Year::latest('year')->first()->year;
-
-		if($this->now > $latest_year)
+		
+		//if there are years saved in the database, check if it is already a new year, then update businesses if it is true
+		elseif($this->now > $latest_year->year)
 		{
+			$latest_year = $latest_year->year;
 
+			while($this->now > $latest_year)
+			{
+				$latest_year++;
+			}
+
+			$year = new Year;
+			$year->year = $latest_year;
+			$year->save();
+
+			//reset fields related to renewal
+			DB::table('businesses')
+				->update([
+					'inspection_count' => 5,
+					'inspection_date' => null,
+					're_inspection_date' => null,
+					'due_date' => null
+			]);
+
+			DB::table('business_requirements')
+				->update([
+					'requirement_params_value' => null,
+					'complied' => false
+			]);
+
+			//remove non-mandatory requirements
+			DB::table('requirements')
+				->where('mandatory', '=', false)
+				->delete();
+
+			//reset remarks
+			DB::table('remarks')->truncate();
 		}
 	}
 }
